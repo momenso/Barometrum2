764791f2e83664aa11ce28957eefba940035c7bc
David Momenso <momenso@gmail.com>
2012-10-28 12:27:54 -0200

    trace remove

--- src/momenso/barometrum2/SettingsActivity.java
+++ src/momenso/barometrum2/SettingsActivity.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 
 public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
@@ -32,8 +31,6 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         final Context context = getApplicationContext();
         final ReadingsData pressureData = ReadingsData.getInstance(context);
-        
-        Log.i("TRACE", "Change settings: " + key);
         
         if (key.equals("BarometerMode")) {
         	updateModeSetting(pressureData);
